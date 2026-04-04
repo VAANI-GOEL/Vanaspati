037491b42399792abe33d324d6882c6d1266fed2
VAANI GUPTA <vaanigupta0808@gmail.com>
2026-04-04 14:26:17 +0530

    updated version....spinner plants

--- app/src/main/java/com/vaanigoel/vanaspati/HomeDashboard.kt
+++ app/src/main/java/com/vaanigoel/vanaspati/HomeDashboard.kt
@@ -3,10 +3,14 @@ package com.vaanigoel.vanaspati
 import com.vaanigoel.vanaspati.databinding.ActivityHomeDashboardBinding
 import android.content.Intent
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
+import android.widget.Spinner
 import android.widget.TextView
-
+import android.widget.*
+import android.view.View
 
 
 class HomeDashboard : AppCompatActivity() {
@@ -18,6 +22,28 @@ class HomeDashboard : AppCompatActivity() {
 
         val tvPlantCount = findViewById<TextView>(R.id.tvPlantCount)
         val btnAddPlant = findViewById<Button>(R.id.btnAddPlant)
+        val spinner = findViewById<Spinner>(R.id.spinnerplants)
+        val plants = arrayOf("Select Plant", "Rose")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, plants)
+        spinner.adapter= adapter
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+
+                if (position == 0) return
+
+                when (position) {
+                    1-> {
+                        val intent = Intent(this@HomeDashboard, RoseActivity::class.java)
+                        startActivity(intent)
+                    }
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {}
+        }
+
 
         //Temporary data
         tvPlantCount.text = "You have 0 plants"
@@ -25,7 +51,6 @@ class HomeDashboard : AppCompatActivity() {
         btnAddPlant.setOnClickListener {
             startActivity(Intent(this, AddPlantActivity::class.java))
         }
-
         }
-
     }
+
